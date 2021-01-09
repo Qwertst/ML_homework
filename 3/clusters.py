78ff891119f249a7bfd61de0cfb68afb831456ce
Qwerty <aydreq@mail.ru>
2021-01-09 19:02:53 +0300

    ???

--- 3/clusters.py
+++ 3/clusters.py
@@ -5,27 +5,32 @@ from sklearn.model_selection import train_test_split
 from sklearn import cluster
 from sklearn.cluster import KMeans
 import pandas as pd
+from sklearn.neighbors import kneighbors_graph
 
-data = pd.read_csv("dataset_1.txt")
+data = pd.read_csv("dataset_3.txt")
 X = data['0'].values
 Y = data['1'].values
 X = np.stack((X, Y), axis = 1)
 nclusters = 2
 plt.scatter(X[:,0],X[:, 1])
+plt.savefig('data_3.png')
 plt.show()
 X = StandardScaler().fit_transform(X)
 X_train, X_test, Y_train, Y_test = train_test_split(X[:, 0], X[:, 1], test_size=0.01, random_state=666)
 New_X = np.stack((X_test, Y_test), axis = 1)
 New_X = StandardScaler().fit_transform(New_X)
 
+connectivity = kneighbors_graph(
+    New_X, n_neighbors=10, include_self=False)
+connectivity = 0.5 * (connectivity + connectivity.T)
+
 hierarchy = cluster.AgglomerativeClustering(
-    linkage="ward", affinity="euclidean",
-    n_clusters=nclusters)
+    linkage="average", affinity="cityblock",
+    n_clusters=nclusters,connectivity=connectivity)
 hierarchy.fit(New_X)
 y_pred = hierarchy.fit_predict(New_X)
 
 plt.scatter(New_X[:, 0], New_X[:, 1], c=y_pred, s=nclusters, cmap='viridis')
-
 sum, cen_x, cen_y = [0] * nclusters, [0] * nclusters, [0] * nclusters
 for x_, y_, i in zip(New_X[:, 0], New_X[:, 1], y_pred):
     sum[i] += 1
@@ -35,9 +40,13 @@ centroids = []
 for i in range(nclusters):
     centroids.append([cen_x[i] / sum[i], cen_y[i] / sum[i]])
 centroids = np.array(centroids)
+plt.scatter(centroids[:, 0], centroids[:, 1], marker="x", color='r')
+plt.savefig('h3.png')
 plt.show()
-
 kmeans = KMeans(init=centroids, n_clusters=nclusters, n_init=1).fit(X)
 y_kmeans = kmeans.fit_predict(X)
+centers = np.array(kmeans.cluster_centers_)
 plt.scatter(X[:, 0], X[:, 1], c=y_kmeans, s=nclusters, cmap='viridis')
+plt.scatter(centers[:, 0], centers[:, 1], marker="x", color='r')
+plt.savefig('k3.png')
 plt.show()
--- 3/create_sample.py
+++ 3/create_sample.py
@@ -7,7 +7,7 @@ n_samples = 100000
 noisy_circles = datasets.make_circles(n_samples=n_samples, factor=.5,
                                       noise=.05)
 noisy_moons = datasets.make_moons(n_samples=n_samples, noise=.05)
-blobs = datasets.make_blobs(n_samples=n_samples, random_state=80,cluster_std=0.4)
+blobs = datasets.make_blobs(n_samples=n_samples, random_state=80,cluster_std=0.2)
 no_structure = np.random.rand(n_samples, 2), None
 random_state = 170
 X, y = datasets.make_blobs(n_samples=n_samples, random_state=random_state)
@@ -17,9 +17,10 @@ aniso = (X_aniso, y)
 varied = datasets.make_blobs(n_samples=n_samples,
                              cluster_std=[1.0, 2.5, 0.5],
                              random_state=random_state)
-dataset=datasets.make_blobs(n_samples=n_samples,centers=10,random_state=666,cluster_std=0.3)
-X, y = dataset
-f = open("dataset_7.txt", "w")
+dataset=datasets.make_blobs(n_samples=n_samples,centers=6,random_state=666,cluster_std=0.1)
+X, y = noisy_moons
+f = open("dataset_3.txt", "w")
+f.write("0,1\n")
 for i in range(n_samples):
     s=str(X[i][0])+","+str(X[i][1])
     f.write("%s\n" % s)
